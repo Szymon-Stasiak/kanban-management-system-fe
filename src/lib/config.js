d5beea8c1863688a234db3a0157fac13f90bb48e
Szymon <stszymek@gmail.com>
2025-12-03 23:45:59 +0100

    api config fix

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,2 +1,3 @@
-// Use a CORS-allowed origin matching backend settings
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "http://127.0.0.4:8000";
+// for Karol - >    LINUX SUCK
+// export const API_BASE_URL = "http://localhost:8000";
