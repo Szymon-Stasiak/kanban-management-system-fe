c8d0224f49817764096a013cbc81e5f3a1b8ae0a
Karol Leszynski <karol.leszynski1@gmail.com>
2025-12-09 21:24:08 +0100

    docker building for debian version

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,2 @@
-export const API_BASE_URL = "http://127.0.0.4:8000";
-// for Karol - >    LINUX SUCK
-// export const API_BASE_URL = "http://localhost:8000";
+// Use a CORS-allowed origin matching backend settings
+export const API_BASE_URL = "http://localhost:8001";
